02b5d1401ab8dab64dc43d02f3c9e881d63a592e
Gaidukov Taras <kemaweyan@gmail.com>
2017-07-06 20:30:38 +0300

    Initial commit

---
+++ test.py
@@ -0,0 +1,125 @@
+#!/usr/bin/env python3
+
+from unittest import main, TestCase, mock
+
+from timertool import _timer
+
+class TestTimerClass(TestCase):
+
+    def setUp(self):
+        self.timer = mock.MagicMock(spec=_timer._timer)
+
+    def test_constructor(self):
+        timer = _timer._timer()
+        self.assertIsNone(timer._time)
+
+    @mock.patch('time.time')
+    def test_start(self, mock_time):
+        _timer._timer.start(self.timer)
+        self.assertEqual(self.timer._start_time, mock_time.return_value)
+
+    def test_stop(self):
+        result = _timer._timer.stop(self.timer)
+        self.assertEqual(result, self.timer._get_time.return_value)
+        self.assertEqual(self.timer._time, self.timer._get_time.return_value)
+
+    def test_enter(self):
+        result = _timer._timer.__enter__(self.timer)
+        self.assertEqual(result, self.timer)
+        self.timer.start.assert_called_with()
+
+    def test_exit(self):
+        _timer._timer.__exit__(self.timer, 0, 0, 0)
+        self.timer.stop.assert_called_with()
+
+    @mock.patch('time.time', return_value=10)
+    def test_get_time(self, mock_time):
+        self.timer._start_time = 3
+        result = _timer._timer._get_time(self.timer)
+        self.assertEqual(result, 7)
+        mock_time.assert_called_with()
+
+    def test_time_property_not_done(self):
+        self.timer._time = None
+        result = _timer._timer.time.fget(self.timer)
+        self.assertEqual(result, self.timer._get_time.return_value)
+        self.timer._get_time.assert_called_with()
+
+    def test_time_property_done(self):
+        self.timer._time = 1
+        result = _timer._timer.time.fget(self.timer)
+        self.assertEqual(result, self.timer._time)
+        self.timer._get_time.assert_not_called()
+
+
+class TestTimerFunc(TestCase):
+
+    @mock.patch.object(_timer, '_timer')
+    def test_timer(self, mock_timer):
+        self.assertEqual(_timer.timer(), mock_timer.return_value)
+        mock_timer.assert_called_with()
+
+
+class TestTimerIntegration(TestCase):
+
+    @mock.patch('time.time')
+    def test_enter_start(self, mock_time):
+        timer = _timer._timer()
+        with timer as t:
+            self.assertEqual(t, timer)
+            self.assertEqual(timer._start_time, mock_time.return_value)
+
+    @mock.patch('time.time', return_value=10)
+    def test_stop_get_time(self, mock_time):
+        timer = _timer._timer()
+        timer._start_time = 2
+        result = timer.stop()
+        self.assertEqual(result, 8)
+        self.assertEqual(timer._time, 8)
+
+    @mock.patch('time.time', return_value=10)
+    def test_exit_stop(self, mock_time):
+        timer = _timer._timer()
+        with timer as t:
+            timer._start_time = 2
+        self.assertEqual(timer._time, 8)
+
+    @mock.patch('time.time', side_effect=[4, 10])
+    def test_enter_exit(self, mock_time):
+        timer = _timer._timer()
+        with timer:
+            pass
+        self.assertEqual(timer._time, 6)
+
+    @mock.patch('time.time', side_effect=[4, 10, 20])
+    def test_time_property(self, mock_time):
+        timer = _timer._timer()
+        with timer as t:
+            self.assertEqual(timer.time, 6)
+        self.assertEqual(timer.time, 16)
+
+    @mock.patch('time.time', side_effect=[4, 10, 20])
+    def test_start_stop(self, mock_time):
+        timer = _timer._timer()
+        timer.start()
+        self.assertEqual(timer.time, 6)
+        timer.stop()
+        self.assertEqual(timer.time, 16)
+
+    @mock.patch('time.time', side_effect=[4, 10, 20])
+    def test_timer_function_start_stop(self, mock_time):
+        timer = _timer.timer()
+        timer.start()
+        self.assertEqual(timer.time, 6)
+        timer.stop()
+        self.assertEqual(timer.time, 16)
+
+    @mock.patch('time.time', side_effect=[4, 10, 20])
+    def test_timer_function_with_statement(self, mock_time):
+        with _timer.timer() as t:
+            self.assertEqual(t.time, 6)
+        self.assertEqual(t.time, 16)
+
+
+if __name__ == "__main__":
+    main()
---
+++ timertool/_timer.py
@@ -0,0 +1,35 @@
+import time
+
+__all__ = ('timer')
+
+class _timer:
+
+    def __init__(self):
+        self._time = None
+
+    def start(self):
+        self._start_time = time.time()
+
+    def stop(self):
+        self._time = self._get_time()
+        return self._time
+
+    def __enter__(self):
+        self.start()
+        return self
+
+    def __exit__(self, exc_type, exc_val, exc_tb):
+        self.stop()
+        
+    def _get_time(self):
+        return time.time() - self._start_time
+
+    @property
+    def time(self):
+        if self._time is not None:
+            return self._time
+        return self._get_time()
+
+
+def timer():
+    return _timer()
